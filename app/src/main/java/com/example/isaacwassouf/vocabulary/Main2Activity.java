b8440354d12d4766daabca2c7cf1dd36d09ac7d7
isaacwassouf <izakwassouf@gmail.com>
2018-05-02 19:39:43 +0300

    Fixed left margin of the source word

--- app/src/main/java/com/example/isaacwassouf/vocabulary/Main2Activity.java
+++ app/src/main/java/com/example/isaacwassouf/vocabulary/Main2Activity.java
@@ -1,11 +1,16 @@
 package com.example.isaacwassouf.vocabulary;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.sax.EndElementListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.net.URI;
+import java.net.URL;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -29,13 +34,35 @@ public class Main2Activity extends AppCompatActivity {
         String engWord= Eng.getText().toString();
         String araWord= Ara.getText().toString();
 
-        if (engWord.isEmpty() || araWord.isEmpty()){
-            Toast.makeText(this, "one of the fields is Empty", Toast.LENGTH_SHORT).show();
+        if (engWord.isEmpty() && araWord.isEmpty()){
+            Toast.makeText(this, "Both of the fields are Empty", Toast.LENGTH_SHORT).show();
             return;
         }
+        else if(engWord.isEmpty()){
+            Toast.makeText(this, "Source word is empty", Toast.LENGTH_SHORT).show();
+        }
+        else if (araWord.isEmpty()){
+            Toast.makeText(this, "you didn\'t enter a meaning", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            database.add(engWord,araWord);
+            Eng.setText("");
+            Ara.setText("");
+        }
+    }
 
-        database.add(engWord,araWord);
-        Eng.setText("");
-        Ara.setText("");
+    public void openBrowser(View view) {
+        String sourceWord= Eng.getText().toString();
+
+        if(sourceWord.isEmpty()){
+            Toast.makeText(this, "you haven\'t entered a word ", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            String url =  "https://translate.google.com/#en/ar/"+sourceWord;
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(url));
+            startActivity(i);
+        }
+
     }
 }
